b1813dcdd243e85dc76247f3eafc710ee7d3145f
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-07-11 16:18:28 +0700

    remove Breadcrumb component

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,23 +1,10 @@
-'use client';
-
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
 import { HomeSidebar } from '@/components/ui/home-sidebar';
 import { Separator } from '@/components/ui/separator';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
-import { useBreadcrumbStore } from '@/hooks/use-breadcrumb';
-import { SlashIcon } from 'lucide-react';
 
 export default function HomeLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  const { breadcrumb } = useBreadcrumbStore();
-
   return (
     <SidebarProvider>
       <HomeSidebar />
@@ -25,22 +12,6 @@ export default function HomeLayout({
         <div className="flex gap-x-2 items-center h-[35px] border-b mb-4 py-1">
           <SidebarTrigger />
           <Separator orientation="vertical" className="min-h-full" />
-
-          <Breadcrumb className="ml-3">
-            <BreadcrumbList>
-              <BreadcrumbItem>
-                <BreadcrumbLink href="/">Home</BreadcrumbLink>
-              </BreadcrumbItem>
-              {breadcrumb.map((bc) => (
-                <>
-                  <BreadcrumbSeparator>
-                    <SlashIcon />
-                  </BreadcrumbSeparator>
-                  <BreadcrumbItem key={bc.url}>{bc.title}</BreadcrumbItem>
-                </>
-              ))}
-            </BreadcrumbList>
-          </Breadcrumb>
         </div>
         {children}
       </main>
