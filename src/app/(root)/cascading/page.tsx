1aec58ffbf3968de6675464c5a558969eafa8879
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-07-14 14:46:59 +0700

    change h1 value

--- src/app/(root)/cascading/page.tsx
+++ src/app/(root)/cascading/page.tsx
@@ -3,7 +3,9 @@ import CascadingTable from './table';
 const CascadingPage = () => {
   return (
     <section className="flex flex-col gap-y-8 w-fit">
-      <h1 className="font-semibold text-foreground text-3xl">Data Cascading</h1>
+      <h1 className="font-semibold text-foreground text-3xl">
+        Daftar Cascading
+      </h1>
       <CascadingTable />
     </section>
   );
