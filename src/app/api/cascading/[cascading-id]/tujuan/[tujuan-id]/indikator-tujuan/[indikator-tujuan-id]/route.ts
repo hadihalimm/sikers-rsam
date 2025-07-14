13231d8d08d3c12301945c5e9724cadbca7b44e2
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-07-14 14:49:11 +0700

    implement api route and query for indikator_tujuan

---
+++ src/app/api/cascading/[cascading-id]/tujuan/[tujuan-id]/indikator-tujuan/[indikator-tujuan-id]/route.ts
@@ -0,0 +1,127 @@
+import db from '@/db';
+import { indikatorTujuan } from '@/db/schema';
+import { and, eq } from 'drizzle-orm';
+import { NextRequest, NextResponse } from 'next/server';
+
+interface RouteParams {
+  params: Promise<{
+    'cascading-id': string;
+    'tujuan-id': string;
+    'indikator-tujuan-id': string;
+  }>;
+}
+
+export async function GET(request: NextRequest, { params }: RouteParams) {
+  try {
+    const { 'tujuan-id': tujuanId, 'indikator-tujuan-id': indikatorTujuanId } =
+      await params;
+
+    if (isNaN(parseInt(tujuanId)) || isNaN(parseInt(indikatorTujuanId))) {
+      return NextResponse.json(
+        { error: 'Invalid ID format. IDs must be integer.' },
+        { status: 400 },
+      );
+    }
+
+    const record = await db.query.indikatorTujuan.findFirst({
+      where: and(
+        eq(indikatorTujuan.id, parseInt(indikatorTujuanId)),
+        eq(indikatorTujuan.tujuanId, parseInt(tujuanId)),
+      ),
+    });
+    if (!record) {
+      return NextResponse.json(
+        { error: "'indikator_tujuan' record not found" },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json(record);
+  } catch (error) {
+    console.error("Error fetching 'indikator_tujuan' record: ", error);
+    return NextResponse.json(
+      { error: "Failed to fetch 'indikator_tujuan' record" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function PUT(request: NextRequest, { params }: RouteParams) {
+  try {
+    const { 'tujuan-id': tujuanId, 'indikator-tujuan-id': indikatorTujuanId } =
+      await params;
+    const body = await request.json();
+    const { nama } = body;
+
+    if (isNaN(parseInt(tujuanId)) || isNaN(parseInt(indikatorTujuanId))) {
+      return NextResponse.json(
+        { error: 'Invalid ID format. IDs must be integer.' },
+        { status: 400 },
+      );
+    }
+
+    const updatedRecord = await db
+      .update(indikatorTujuan)
+      .set({ nama })
+      .where(
+        and(
+          eq(indikatorTujuan.id, parseInt(indikatorTujuanId)),
+          eq(indikatorTujuan.tujuanId, parseInt(tujuanId)),
+        ),
+      )
+      .returning();
+
+    if (updatedRecord.length === 0) {
+      return NextResponse.json(
+        { error: "'indikator_tujuan' record not found" },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json(updatedRecord[0]);
+  } catch (error) {
+    console.error("Error updating 'indikator_tujuan' record: ", error);
+    return NextResponse.json(
+      { error: "Failed to update 'indikator_tujuan' record" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function DELETE(request: NextRequest, { params }: RouteParams) {
+  try {
+    const { 'tujuan-id': tujuanId, 'indikator-tujuan-id': indikatorTujuanId } =
+      await params;
+
+    if (isNaN(parseInt(tujuanId)) || isNaN(parseInt(indikatorTujuanId))) {
+      return NextResponse.json(
+        { error: 'Invalid ID format. IDs must be integer.' },
+        { status: 400 },
+      );
+    }
+
+    const deletedRecord = await db
+      .delete(indikatorTujuan)
+      .where(
+        and(
+          eq(indikatorTujuan.id, parseInt(indikatorTujuanId)),
+          eq(indikatorTujuan.tujuanId, parseInt(tujuanId)),
+        ),
+      )
+      .returning();
+
+    if (deletedRecord.length === 0) {
+      return NextResponse.json(
+        { error: "'indikator_tujuan' record not found" },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json({
+      message: "'indikator_tujuan' record deleted successfully",
+    });
+  } catch (error) {
+    console.error("Error deleting 'indikator_tujuan' record: ", error);
+    return NextResponse.json(
+      { error: "Failed to delete 'indikator_tujuan' record" },
+      { status: 500 },
+    );
+  }
+}
---
+++ src/app/api/cascading/[cascading-id]/tujuan/[tujuan-id]/indikator-tujuan/route.ts
@@ -0,0 +1,33 @@
+import db from '@/db';
+import { indikatorTujuan } from '@/db/schema';
+import { NextRequest, NextResponse } from 'next/server';
+
+interface RouteParams {
+  params: Promise<{
+    'cascading-id': string;
+    'tujuan-id': string;
+  }>;
+}
+
+export async function POST(request: NextRequest, { params }: RouteParams) {
+  try {
+    // const { 'cascading-id': cascadingId, 'tujuan-id': tujuanId } = await params;
+    const body = await request.json();
+    const { nama, tujuanId } = body;
+
+    const newRecord = await db
+      .insert(indikatorTujuan)
+      .values({
+        nama,
+        tujuanId,
+      })
+      .returning();
+    return NextResponse.json(newRecord[0], { status: 201 });
+  } catch (error) {
+    console.error("Error creating 'indikator_tujuan' records: ", error);
+    return NextResponse.json({
+      error: "Failed to create 'indikator_tujuan' record",
+      status: 500,
+    });
+  }
+}
