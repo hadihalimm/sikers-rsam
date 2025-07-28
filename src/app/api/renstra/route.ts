fc06b15788ee7a61150c95510e415b74d48a09a3
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-07-28 10:07:00 +0700

    initialize related data after creating renstra

--- src/app/api/renstra/route.ts
+++ src/app/api/renstra/route.ts
@@ -1,5 +1,11 @@
 import db from '@/db';
-import { renstra } from '@/db/schema';
+import {
+  cascading,
+  indikatorSasaranTarget,
+  indikatorTujuanTarget,
+  renstra,
+} from '@/db/schema';
+import { eq } from 'drizzle-orm';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET() {
@@ -30,6 +36,51 @@ export async function POST(request: NextRequest) {
         cascadingId,
       })
       .returning();
+
+    const cascadingRecord = await db.query.cascading.findFirst({
+      where: eq(cascading.id, cascadingId),
+      with: {
+        tujuanList: {
+          with: {
+            indikatorTujuanList: true,
+            sasaranList: {
+              with: {
+                indikatorSasaranList: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    if (!cascadingRecord) {
+      return NextResponse.json(
+        { error: "related 'cascading' record not found" },
+        { status: 404 },
+      );
+    }
+    for (const tujuan of cascadingRecord.tujuanList) {
+      for (const indikator of tujuan.indikatorTujuanList) {
+        await db.insert(indikatorTujuanTarget).values({
+          indikatorTujuanId: indikator.id,
+          renstraId: newRecord[0].id,
+        });
+      }
+      for (const sasaran of tujuan.sasaranList) {
+        for (const indikator of sasaran.indikatorSasaranList) {
+          for (
+            let i = cascadingRecord.tahunMulai;
+            i <= cascadingRecord.tahunBerakhir;
+            i++
+          ) {
+            await db.insert(indikatorSasaranTarget).values({
+              tahun: i,
+              indikatorSasaranId: indikator.id,
+              renstraId: newRecord[0].id,
+            });
+          }
+        }
+      }
+    }
 
     return NextResponse.json(newRecord[0], { status: 201 });
   } catch (error) {
