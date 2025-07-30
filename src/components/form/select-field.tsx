677e7ae234b9b0d38943a29e4ae4e9bf22f9a610
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-07-30 07:42:23 +0700

    implement SelectField for form

--- src/components/form/select-field.tsx
+++ src/components/form/select-field.tsx
@@ -8,7 +8,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from '../ui/select';
-
 export interface SelectOption {
   label: string;
   value: string;
@@ -20,6 +19,7 @@ interface SelectFieldProps {
   placeholder?: string;
   disabled?: boolean;
   className?: string;
+  onValueChange?: (value: string) => void;
 }
 
 const SelectField = ({
@@ -28,19 +28,27 @@ const SelectField = ({
   placeholder,
   disabled,
   className,
+  onValueChange,
 }: SelectFieldProps) => {
   const field = useFieldContext<string>();
+  const handleValueChange = (value: string) => {
+    field.handleChange(value);
+    onValueChange?.(value);
+  };
   return (
     <div className="flex flex-col gap-y-1">
       <Label className="ml-1">{label}</Label>
       <Select
         value={field.state.value}
-        onValueChange={(value) => field.handleChange(value)}
+        onValueChange={handleValueChange}
         disabled={disabled}>
-        <SelectTrigger className={cn('w-full', className)}>
-          <SelectValue placeholder={placeholder} />
+        <SelectTrigger className={cn('w-full !h-auto', className)}>
+          <div className="text-start">
+            <SelectValue placeholder={placeholder} />
+          </div>
         </SelectTrigger>
-        <SelectContent>
+
+        <SelectContent className="w-full max-w-[var(--radix-select-trigger-width)]">
           {options.map((option, index) => (
             <SelectItem key={index} value={option.value}>
               {option.label}
