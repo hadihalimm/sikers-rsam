09b06152f74a2c5b0f7d892b243441a0cb0b83c7
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-08-04 11:17:32 +0700

    add h2 heading

--- src/app/(root)/indikator-kinerja-utama/[indikator-kinerja-utama-id]/iku-detail/page.tsx
+++ src/app/(root)/indikator-kinerja-utama/[indikator-kinerja-utama-id]/iku-detail/page.tsx
@@ -1,9 +1,20 @@
+'use client';
+
+import { useGetIndikatorKinerjaUtama } from '@/hooks/query/indikator-kinerja-utama/indikator-kinerja-utama';
 import IndikatorKinerjaUtamaDetailTable from './table';
+import { useParams } from 'next/navigation';
 
 const IndikatorKinerjaUtamaDetailPage = () => {
+  const params = useParams();
+  const { data: iku } = useGetIndikatorKinerjaUtama(
+    Number(params['indikator-kinerja-utama-id']),
+  );
   return (
     <section className="flex flex-col gap-y-8 w-fit">
-      <h1 className="font-semibold text-foreground text-3xl">Detail IKU</h1>
+      <h1 className="font-semibold text-foreground text-3xl">
+        Detail Indikator Kinerja Utama
+      </h1>
+      <h2>{iku?.nama}</h2>
       <IndikatorKinerjaUtamaDetailTable />
     </section>
   );
