5c5fcb6d6d212bb80d515a5bb166de87ec9da093
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-08-04 11:16:45 +0700

    separate component

--- src/app/(root)/indikator-kinerja-utama/[indikator-kinerja-utama-id]/iku-detail/[iku-detail-id]/page.tsx
+++ src/app/(root)/indikator-kinerja-utama/[indikator-kinerja-utama-id]/iku-detail/[iku-detail-id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
-import TipTapTextEditor from '@/components/tiptap/tiptap';
 import { useGetIkuDetail } from '@/hooks/query/indikator-kinerja-utama/iku-detail';
 import { useParams } from 'next/navigation';
+import IkuDetailUpdateForm from './form';
 
 const IkuDetailEditPage = () => {
   const params = useParams();
@@ -9,38 +9,23 @@ const IkuDetailEditPage = () => {
     Number(params['indikator-kinerja-utama-id']),
     Number(params['iku-detail-id']),
   );
+
   if (!data) return null;
   return (
     <section className="flex flex-col gap-y-8">
       <h1 className="font-semibold text-foreground text-3xl">
         Edit Detail IKU
       </h1>
-
-      <div className="flex flex-col gap-y-4">
-        <div className="flex gap-x-8 justify-between">
-          <div className="flex flex-col gap-y-1 w-full">
-            <h2>Baseline</h2>
-            <TipTapTextEditor
-              content={data.baseline ?? { type: 'doc', content: [] }}
-              onChange={(content) => console.log(content)}
-            />
-          </div>
-          <div className="flex flex-col gap-y-1 w-full">
-            <h2>Penanggung jawab</h2>
-            <TipTapTextEditor
-              content={data.penanggungJawab ?? { type: 'doc', content: [] }}
-              onChange={(content) => console.log(content)}
-            />
-          </div>
-        </div>
-        <div className="flex flex-col gap-y-1">
-          <h2>Penjelasan</h2>
-          <TipTapTextEditor
-            content={data.penjelasan ?? { type: 'doc', content: [] }}
-            onChange={(content) => console.log(content)}
-          />
-        </div>
-      </div>
+      <IkuDetailUpdateForm
+        data={data}
+        indikatorKinerjaUtamaId={Number(params['indikator-kinerja-utama-id'])}
+        ikuDetailId={Number(params['iku-detail-id'])}
+        onSuccess={() => {
+          window.location.href = `/indikator-kinerja-utama/${Number(
+            params['indikator-kinerja-utama-id'],
+          )}/iku-detail`;
+        }}
+      />
     </section>
   );
 };
