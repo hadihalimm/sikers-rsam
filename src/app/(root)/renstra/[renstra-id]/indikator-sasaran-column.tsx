c9ca8d63c79feb2d58932c2a7f539eafb1d6b448
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-08-12 07:52:45 +0700

    change view to table

--- src/app/(root)/renstra/[renstra-id]/indikator-sasaran-column.tsx
+++ src/app/(root)/renstra/[renstra-id]/indikator-sasaran-column.tsx
@@ -1,5 +1,13 @@
 import { Button } from '@/components/ui/button';
 import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import {
   IndikatorSasaran,
   IndikatorSasaranTarget,
   Kegiatan,
@@ -11,7 +19,7 @@ import {
   Sasaran,
   SubKegiatan,
 } from '@/types/database';
-import { ArrowRight, Plus, Trash } from 'lucide-react';
+import { Plus, Trash } from 'lucide-react';
 
 type SasaranWithTargets = Sasaran & {
   indikatorSasaranList: (IndikatorSasaran & {
@@ -44,7 +52,7 @@ const IndikatorSasaranColumn = ({
   onDeleteProgram,
 }: IndikatorSasaranColumnProps) => {
   return (
-    <div className="flex flex-col gap-y-8">
+    <div className="flex flex-col gap-y-6">
       {sasaranList.map((sasaran, index) => (
         <div
           key={sasaran.id}
@@ -52,12 +60,31 @@ const IndikatorSasaranColumn = ({
           <p className="font-medium text-base text-foreground">
             {index + 1}. {sasaran.judul}
           </p>
-          {sasaran.indikatorSasaranList.map((indikator) => (
-            <div key={indikator.id} className="flex gap-x-2">
-              <p className="flex-1/2">Indikator: {indikator.nama}</p>
-              <div className="flex-1/2 flex flex-col gap-y-1">
-                <div className="flex gap-x-5 items-center">
-                  <p>Target</p>
+          <div className="flex flex-col gap-y-4">
+            {sasaran.indikatorSasaranList.map((indikator) => (
+              <div key={indikator.id} className="flex flex-col">
+                <p className="text-base">Indikator: {indikator.nama}</p>
+                <div className="flex gap-x-2 w-fit">
+                  <Table className="w-2/3 max-w-full">
+                    <TableHeader>
+                      <TableRow>
+                        {indikator.indikatorSasaranTargetList.map((target) => (
+                          <TableHead key={target.tahun} className="border">
+                            {target.tahun}
+                          </TableHead>
+                        ))}
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      <TableRow>
+                        {indikator.indikatorSasaranTargetList.map((target) => (
+                          <TableCell key={target.target} className="border">
+                            {target.target}
+                          </TableCell>
+                        ))}
+                      </TableRow>
+                    </TableBody>
+                  </Table>
                   <Button
                     size="sm"
                     className="w-10 h-5 text-xs"
@@ -67,52 +94,50 @@ const IndikatorSasaranColumn = ({
                     Edit
                   </Button>
                 </div>
-                {indikator.indikatorSasaranTargetList.map((target) => (
-                  <div key={target.id} className="flex gap-x-2 items-center">
-                    <p>{target.tahun}</p>
-                    <ArrowRight size={15} />
-                    <p>{target.target}</p>
-                  </div>
-                ))}
               </div>
-            </div>
-          ))}
-          <Button
-            className="w-fit h-6 text-xs"
-            size="sm"
-            onClick={() => onAddProgram(sasaran)}>
-            <Plus />
-            <p>Program</p>
-          </Button>
+            ))}
+          </div>
 
-          {sasaran.programSasaranList.map((ps, index) => (
-            <div
-              key={ps.id}
-              className="flex flex-col gap-y-1 border rounded-lg p-2">
-              <div className="flex gap-x-2 justify-between">
-                <p className="font-medium">
-                  Program {index + 1} : {ps.program.refProgram.nama}
-                </p>
-                <Button
-                  className="w-6 h-6 text-xs"
-                  variant="destructive"
-                  size="sm"
-                  onClick={() => {
-                    onDeleteProgram(ps);
-                  }}>
-                  <Trash />
-                </Button>
-              </div>
-              <div className="ml-2">
-                <p>Kegiatan: {ps.program.kegiatan.refKegiatan.nama}</p>
-                <ul className="list-disc ml-4">
-                  {ps.program.kegiatan.subKegiatanList.map((sub) => (
-                    <li key={sub.id}>{sub.refSubKegiatan.nama}</li>
-                  ))}
-                </ul>
-              </div>
+          <div className="flex flex-col gap-y-1">
+            <div className="flex justify-end">
+              <Button
+                className="w-fit h-6 text-xs"
+                size="sm"
+                onClick={() => onAddProgram(sasaran)}>
+                <Plus />
+                <p>Program</p>
+              </Button>
             </div>
-          ))}
+
+            {sasaran.programSasaranList.map((ps, index) => (
+              <div
+                key={ps.id}
+                className="flex flex-col gap-y-1 border rounded-lg p-2">
+                <div className="flex gap-x-2 justify-between">
+                  <p className="font-medium">
+                    Program {index + 1} : {ps.program.refProgram.nama}
+                  </p>
+                  <Button
+                    className="w-6 h-6 text-xs"
+                    variant="destructive"
+                    size="sm"
+                    onClick={() => {
+                      onDeleteProgram(ps);
+                    }}>
+                    <Trash />
+                  </Button>
+                </div>
+                <div className="ml-2">
+                  <p>Kegiatan: {ps.program.kegiatan.refKegiatan.nama}</p>
+                  <ul className="list-disc ml-4">
+                    {ps.program.kegiatan.subKegiatanList.map((sub) => (
+                      <li key={sub.id}>{sub.refSubKegiatan.nama}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
+          </div>
           {/* {index !== sasaranList.length - 1 && <Separator className="mt-4" />} */}
         </div>
       ))}
