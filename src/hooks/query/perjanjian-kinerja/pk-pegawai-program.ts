9e9eb11775b689a0fbaf403af4eeac9109334305
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-08-15 10:37:35 +0700

    implement query hooks for perjanjianKinerja

---
+++ src/hooks/query/perjanjian-kinerja/pk-pegawai-program.ts
@@ -0,0 +1,107 @@
+import api from '@/lib/axios';
+import { getQueryClient } from '@/lib/get-query-client';
+import { PerjanjianKinerjaPegawaiProgram } from '@/types/database';
+import { useMutation, useQuery } from '@tanstack/react-query';
+
+export const useGetAllPkPegawaiProgram = (
+  pkId: number,
+  pkPegawaiId: number,
+) => {
+  return useQuery<PerjanjianKinerjaPegawaiProgram[]>({
+    queryKey: ['pk-pegawai-program', pkId, pkPegawaiId],
+    queryFn: async () => {
+      const { data } = await api.get(
+        `perjanjian-kinerja/${pkId}/pk-pegawai/${pkPegawaiId}/pk-pegawai-program`,
+      );
+      return data;
+    },
+  });
+};
+
+export const useGetPkPegawaiProgram = (
+  pkId: number,
+  pkPegawaiId: number,
+  pkPegawaiProgramId: number,
+) => {
+  return useQuery<PerjanjianKinerjaPegawaiProgram>({
+    queryKey: ['pk-pegawai-program', pkId, pkPegawaiId, pkPegawaiProgramId],
+    queryFn: async () => {
+      const { data } = await api.get(
+        `perjanjian-kinerja/${pkId}/pk-pegawai/${pkPegawaiId}/pk-pegawai-program/${pkPegawaiProgramId}`,
+      );
+      return data;
+    },
+  });
+};
+
+export const useCreatePkPegawaiSasaran = (
+  pkId: number,
+  pkPegawaiId: number,
+) => {
+  const queryClient = getQueryClient();
+  return useMutation({
+    mutationFn: async (newRecord: {
+      subKegiatanId: number;
+      anggaran: number;
+      sasaranId: number;
+    }) => {
+      const { data } = await api.post(
+        `perjanjian-kinerja/${pkId}/pk-pegawai/${pkPegawaiId}/pk-pegawai-program`,
+        newRecord,
+      );
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['pk-pegawai-program', pkId, pkPegawaiId],
+      });
+    },
+  });
+};
+
+export const useUpdatePkPegawaiProgram = (
+  pkId: number,
+  pkPegawaiId: number,
+) => {
+  const queryClient = getQueryClient();
+  return useMutation({
+    mutationFn: async (updatedRecord: { id: number; anggaran: number }) => {
+      const { data } = await api.put(
+        `perjanjian-kinerja/${pkId}/pk-pegawai/${pkPegawaiId}/pk-pegawai-program/${updatedRecord.id}`,
+        updatedRecord,
+      );
+      return data;
+    },
+    onSuccess: (_data, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: ['pk-pegawai-program', pkId, pkPegawaiId],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ['pk-pegawai-program', pkId, pkPegawaiId, variables.id],
+      });
+    },
+  });
+};
+
+export const useDeletePkPegawaiProgram = (
+  pkId: number,
+  pkPegawaiId: number,
+) => {
+  const queryClient = getQueryClient();
+  return useMutation({
+    mutationFn: async (id: number) => {
+      const { data } = await api.put(
+        `perjanjian-kinerja/${pkId}/pk-pegawai/${pkPegawaiId}/pk-pegawai-program/${id}`,
+      );
+      return data;
+    },
+    onSuccess: (_data, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: ['pk-pegawai-program', pkId, pkPegawaiId],
+      });
+      queryClient.removeQueries({
+        queryKey: ['pk-pegawai-program', pkId, pkPegawaiId, variables],
+      });
+    },
+  });
+};
---
+++ src/hooks/query/perjanjian-kinerja/pk-pegawai-sasaran.ts
@@ -0,0 +1,114 @@
+import api from '@/lib/axios';
+import { getQueryClient } from '@/lib/get-query-client';
+import {
+  PerjanjianKinerjaPegawaiSasaran,
+  PerjanjianKinerjaPegawaiSasaranDetail,
+} from '@/types/database';
+import { useMutation, useQuery } from '@tanstack/react-query';
+
+export const useGetAllPkPegawaiSasaran = (
+  pkId: number,
+  pkPegawaiId: number,
+) => {
+  return useQuery<PerjanjianKinerjaPegawaiSasaranDetail[]>({
+    queryKey: ['pk-pegawai-sasaran', pkId, pkPegawaiId],
+    queryFn: async () => {
+      const { data } = await api.get(
+        `perjanjian-kinerja/${pkId}/pk-pegawai/${pkPegawaiId}/pk-pegawai-sasaran`,
+      );
+      return data;
+    },
+  });
+};
+
+export const useGetPkPegawaiSasaran = (
+  pkId: number,
+  pkPegawaiId: number,
+  pkPegawaiSasaranId: number,
+) => {
+  return useQuery<PerjanjianKinerjaPegawaiSasaran>({
+    queryKey: ['pk-pegawai-sasaran', pkId, pkPegawaiId, pkPegawaiSasaranId],
+    queryFn: async () => {
+      const { data } = await api.get(
+        `perjanjian-kinerja/${pkId}/pk-pegawai/${pkPegawaiId}/pk-pegawai-sasaran/${pkPegawaiSasaranId}`,
+      );
+      return data;
+    },
+  });
+};
+
+export const useCreatePkPegawaiSasaran = (
+  pkId: number,
+  pkPegawaiId: number,
+) => {
+  const queryClient = getQueryClient();
+  return useMutation({
+    mutationFn: async (newRecord: {
+      target: string;
+      modelCapaian: number;
+      indikatorSasaranId: number;
+    }) => {
+      const { data } = await api.post(
+        `perjanjian-kinerja/${pkId}/pk-pegawai/${pkPegawaiId}/pk-pegawai-sasaran`,
+        newRecord,
+      );
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['pk-pegawai-sasaran', pkId, pkPegawaiId],
+      });
+    },
+  });
+};
+
+export const useUpdatePkPegawaiSasaran = (
+  pkId: number,
+  pkPegawaiId: number,
+) => {
+  const queryClient = getQueryClient();
+  return useMutation({
+    mutationFn: async (updatedRecord: {
+      id: number;
+      target: string;
+      modelCapaian: number;
+    }) => {
+      const { data } = await api.put(
+        `perjanjian-kinerja/${pkId}/pk-pegawai/${pkPegawaiId}/pk-pegawai-sasaran/${updatedRecord.id}`,
+        updatedRecord,
+      );
+      return data;
+    },
+    onSuccess: (_data, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: ['pk-pegawai-sasaran', pkId, pkPegawaiId],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ['pk-pegawai-sasaran', pkId, pkPegawaiId, variables.id],
+      });
+    },
+  });
+};
+
+export const useDeletePkPegawaiSasaran = (
+  pkId: number,
+  pkPegawaiId: number,
+) => {
+  const queryClient = getQueryClient();
+  return useMutation({
+    mutationFn: async (id: number) => {
+      const { data } = await api.put(
+        `perjanjian-kinerja/${pkId}/pk-pegawai/${pkPegawaiId}/pk-pegawai-sasaran/${id}`,
+      );
+      return data;
+    },
+    onSuccess: (_data, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: ['pk-pegawai-sasaran', pkId, pkPegawaiId],
+      });
+      queryClient.removeQueries({
+        queryKey: ['pk-pegawai-sasaran', pkId, pkPegawaiId, variables],
+      });
+    },
+  });
+};
