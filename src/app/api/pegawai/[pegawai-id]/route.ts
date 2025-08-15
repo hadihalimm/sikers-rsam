522f8eb2f8f2340a45e534d53ab0aab38cabdab8
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-08-15 10:37:23 +0700

    implement api routes and query hooks for pegawai

---
+++ src/app/api/pegawai/[pegawai-id]/route.ts
@@ -0,0 +1,86 @@
+import db from '@/db';
+import { pegawai } from '@/db/schema';
+import { eq } from 'drizzle-orm';
+import { NextRequest, NextResponse } from 'next/server';
+
+interface RouteParams {
+  params: Promise<{
+    'pegawai-id': string;
+  }>;
+}
+
+export async function GET(request: NextRequest, { params }: RouteParams) {
+  try {
+    const { 'pegawai-id': pegawaiId } = await params;
+    const record = await db.query.pegawai.findFirst({
+      where: eq(pegawai.id, parseInt(pegawaiId)),
+    });
+    if (!record) {
+      return NextResponse.json(
+        { error: "'pegawai' record not found" },
+        { status: 404 },
+      );
+    }
+  } catch (error) {
+    console.error("Error fetching 'pegawai' record: ", error);
+    return NextResponse.json(
+      { error: "Failed to fetch 'pegawai' record" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function PUT(request: NextRequest, { params }: RouteParams) {
+  try {
+    const { 'pegawai-id': pegawaiId } = await params;
+    const body = await request.json();
+    const { nama, jabatan, userId } = body;
+
+    const updatedRecord = await db
+      .update(pegawai)
+      .set({ nama, jabatan, userId })
+      .where(eq(pegawai.id, parseInt(pegawaiId)))
+      .returning();
+
+    if (updatedRecord.length === 0) {
+      return NextResponse.json(
+        { error: "'pegawai' record not found" },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json(updatedRecord[0]);
+  } catch (error) {
+    console.error("Error updating 'pegawai' record: ", error);
+    return NextResponse.json(
+      { error: "Failed to update 'pegawai' record" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function DELETE(request: NextRequest, { params }: RouteParams) {
+  try {
+    const { 'pegawai-id': pegawaiId } = await params;
+
+    const deletedRecord = await db
+      .delete(pegawai)
+      .where(eq(pegawai.id, parseInt(pegawaiId)))
+      .returning();
+    if (deletedRecord.length === 0) {
+      return NextResponse.json(
+        { error: "'pegawai' record not found" },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json({
+      message: "'pegawai' record deleted successfully",
+      deletedRecord: deletedRecord[0],
+    });
+  } catch (error) {
+    console.error("Error deleting 'pegawai' record: ", error);
+    return NextResponse.json(
+      { error: "Failed to delete 'pegawai' record" },
+      { status: 500 },
+    );
+  }
+}
---
+++ src/hooks/query/pegawai/pegawai.ts
@@ -0,0 +1,84 @@
+import api from '@/lib/axios';
+import { getQueryClient } from '@/lib/get-query-client';
+import { Pegawai } from '@/types/database';
+import { useMutation, useQuery } from '@tanstack/react-query';
+
+export const useGetAllPegawai = (userId: number) => {
+  return useQuery<Pegawai[]>({
+    queryKey: ['pegawai-list', userId],
+    queryFn: async () => {
+      const { data } = await api.get(`/pegawai?userId=${userId}`);
+      return data;
+    },
+  });
+};
+
+export const useGetPegawai = (id: number) => {
+  return useQuery<Pegawai>({
+    queryKey: ['pegawai', id],
+    queryFn: async () => {
+      const { data } = await api.get(`/pegawai/${id}`);
+      return data;
+    },
+  });
+};
+
+export const useCreatePegawai = () => {
+  const queryClient = getQueryClient();
+  return useMutation({
+    mutationFn: async (newPegawai: {
+      nama: string;
+      jabatan: string;
+      userId: string;
+    }) => {
+      const { data } = await api.post(`/pegawai`, newPegawai);
+      return data;
+    },
+    onSuccess: (_data, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: ['pegawai-list', variables.userId],
+      });
+    },
+  });
+};
+
+export const useUpdatePegawai = () => {
+  const queryClient = getQueryClient();
+  return useMutation({
+    mutationFn: async (updatedItem: {
+      id: number;
+      nama: string;
+      jabatan: string;
+      userId: string;
+    }) => {
+      const { data } = await api.put(`/pegawai/${updatedItem.id}`);
+      return data;
+    },
+    onSuccess: (_data, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: ['pegawai-list', variables.userId],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ['pegawai', variables.id],
+      });
+    },
+  });
+};
+
+export const useDeletePegawai = () => {
+  const queryClient = getQueryClient();
+  return useMutation({
+    mutationFn: async (id: number) => {
+      const { data } = await api.delete(`/pegawai/${id}`);
+      return data;
+    },
+    onSuccess: (data, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: ['pegawai-list', data.deletedRecord.userId],
+      });
+      queryClient.removeQueries({
+        queryKey: ['pegawai', variables],
+      });
+    },
+  });
+};
