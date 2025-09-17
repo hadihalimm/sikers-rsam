80389118182de338f38b32108f0b88a2975b91c5
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-09-17 11:40:46 +0700

    implement data groping

--- src/app/(root)/rencana-aksi/[ra-id]/ra-pegawai/[ra-pegawai-id]/ra-detail/target-subkegiatan-column.tsx
+++ src/app/(root)/rencana-aksi/[ra-id]/ra-pegawai/[ra-pegawai-id]/ra-detail/target-subkegiatan-column.tsx
@@ -1,92 +1,100 @@
 import { Button } from '@/components/ui/button';
 import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import {
   PerjanjianKinerjaPegawaiProgram,
-  RefKegiatan,
-  RefProgram,
-  RefSubKegiatan,
   RencanaAksiSubKegiatanTarget,
   RencanaAksiSubkegiatanTargetDetail,
-  Satuan,
 } from '@/types/database';
+import { groupBy } from 'lodash';
 import { Edit, Plus, Trash } from 'lucide-react';
 
 interface RencanaAksiSubkegiatanTargetColumnProps {
   data: RencanaAksiSubkegiatanTargetDetail[];
-  onEdit: (data?: RencanaAksiSubKegiatanTarget) => void;
+  onEdit: (
+    data?: RencanaAksiSubKegiatanTarget,
+    pkPegawaiProgram?: PerjanjianKinerjaPegawaiProgram,
+  ) => void;
   onDelete: (data: RencanaAksiSubKegiatanTarget) => void;
 }
-
-type GroupedData = {
-  refProgram: RefProgram;
-  refKegiatan: RefKegiatan;
-  refSubKegiatan: RefSubKegiatan;
-  perjanjianKinerjaPegawaiProgram: PerjanjianKinerjaPegawaiProgram;
-  satuan: Satuan;
-  targetList: RencanaAksiSubKegiatanTarget[];
-};
 
 const RencanaAksiSubkegiatanTargetColumn = ({
   data,
   onEdit,
   onDelete,
 }: RencanaAksiSubkegiatanTargetColumnProps) => {
-  const grouped: GroupedData = {
-    refProgram: data[0]?.refProgram,
-    refKegiatan: data[0]?.refKegiatan,
-    refSubKegiatan: data[0]?.refSubKegiatan,
-    perjanjianKinerjaPegawaiProgram: data[0]?.perjanjianKinerjaPegawaiProgram,
-    satuan: data[0]?.satuan,
-    targetList: data
-      .map((item) => item.rencanaAksiSubKegiatanTarget)
-      .filter((item) => item),
-  };
+  const grouped = groupBy(data, (item) => item.refSubKegiatan.id);
   return (
-    <div className="flex flex-col gap-y-4">
-      <div className="flex flex-col gap-y-2">
-        <div>
-          <p>{grouped.refProgram?.nama}</p>
-          <p>{grouped.refKegiatan?.nama}</p>
-          <p>{grouped.refSubKegiatan?.nama}</p>
-          <p>Anggaran: {grouped.perjanjianKinerjaPegawaiProgram?.anggaran}</p>
-        </div>
-        <div className="flex flex-col gap-y-4">
-          {grouped.targetList.map((target, index) => (
-            <div key={target.id} className="flex justify-between">
-              <div>
-                <p>
-                  {index + 1}. {target?.nama}
-                </p>
-                <p>
-                  {target?.target} {grouped.satuan?.nama}
-                </p>
-              </div>
-              <div className="flex flex-col gap-y-1">
-                <Button
-                  className="w-7 h-6"
-                  onClick={() => {
-                    onEdit(target);
-                  }}>
-                  <Edit />
-                </Button>
-                <Button
-                  className="w-7 h-6"
-                  variant="destructive"
-                  onClick={() => {
-                    onDelete(target);
-                  }}>
-                  <Trash />
-                </Button>
-              </div>
+    <div className="flex flex-col gap-y-8">
+      {Object.values(grouped).map((items) => {
+        const first = items[0];
+        return (
+          <div key={first.refSubKegiatan.id} className="flex flex-col gap-y-6">
+            <div>
+              <p className="font-semibold">{first.refProgram?.nama}</p>
+              <p>Kegiatan: {first.refKegiatan?.nama}</p>
+              <p>Sub-kegiatan: {first.refSubKegiatan?.nama}</p>
             </div>
-          ))}
-        </div>
-      </div>
-      <Button
-        className="w-fit h-6 bg-green-500 hover:bg-green-500/90"
-        onClick={() => onEdit()}>
-        <Plus />
-        Tambah Indikator
-      </Button>
+            <div className="flex flex-col gap-y-2">
+              <p>Anggaran: {first.perjanjianKinerjaPegawaiProgram.anggaran}</p>
+              <Table className="table-fixed">
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="w-48">
+                      Indikator Output Kegiatan
+                    </TableHead>
+                    <TableHead className="w-18">Target</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {items.map((item) => (
+                    <TableRow
+                      key={`sub-${item.refSubKegiatan?.id}-target-${item.rencanaAksiSubKegiatanTarget?.id}`}>
+                      <TableCell>
+                        {item.rencanaAksiSubKegiatanTarget?.nama}
+                      </TableCell>
+                      <TableCell>
+                        {item.rencanaAksiSubKegiatanTarget?.target}
+                      </TableCell>
+                      <TableCell className="flex flex-col gap-y-1">
+                        <Button
+                          className="w-7 h-6"
+                          onClick={() => {
+                            onEdit(item.rencanaAksiSubKegiatanTarget);
+                          }}>
+                          <Edit />
+                        </Button>
+                        <Button
+                          className="w-7 h-6"
+                          variant="destructive"
+                          onClick={() => {
+                            onDelete(item.rencanaAksiSubKegiatanTarget);
+                          }}>
+                          <Trash />
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+              <Button
+                className="w-fit h-6 bg-green-500 hover:bg-green-500/90"
+                onClick={() =>
+                  onEdit(undefined, first.perjanjianKinerjaPegawaiProgram)
+                }>
+                <Plus />
+                Tambah Indikator
+              </Button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
