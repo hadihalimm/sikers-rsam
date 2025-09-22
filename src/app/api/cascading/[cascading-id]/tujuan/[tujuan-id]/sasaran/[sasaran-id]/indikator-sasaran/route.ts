f70d3114c427eacb2d7c0e812db8152a03bc5cf6
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-09-22 08:02:43 +0700

    automatically insert related row

--- src/app/api/cascading/[cascading-id]/tujuan/[tujuan-id]/sasaran/[sasaran-id]/indikator-sasaran/route.ts
+++ src/app/api/cascading/[cascading-id]/tujuan/[tujuan-id]/sasaran/[sasaran-id]/indikator-sasaran/route.ts
@@ -1,5 +1,14 @@
 import db from '@/db';
-import { indikatorSasaran } from '@/db/schema';
+import {
+  cascading,
+  indikatorKinerjaUtama,
+  indikatorKinerjaUtamaDetail,
+  indikatorSasaran,
+  indikatorSasaranTarget,
+  renstra,
+  sasaran,
+  tujuan,
+} from '@/db/schema';
 import { getCurrentSession } from '@/lib/user';
 import { eq } from 'drizzle-orm';
 import { NextRequest, NextResponse } from 'next/server';
@@ -46,6 +55,65 @@ export async function POST(request: NextRequest, { params }: RouteParams) {
       .insert(indikatorSasaran)
       .values({ nama, sasaranId: parseInt(sasaranId) })
       .returning();
+
+    const sasaranRecord = await db.query.sasaran.findFirst({
+      where: eq(sasaran.id, parseInt(sasaranId)),
+    });
+
+    if (sasaranRecord?.level === 1) {
+      const cascadingRecord = await db
+        .selectDistinctOn([cascading.id], {
+          id: cascading.id,
+          tahunMulai: cascading.tahunMulai,
+          tahunBerakhir: cascading.tahunBerakhir,
+        })
+        .from(sasaran)
+        .where(eq(sasaran.id, parseInt(sasaranId)))
+        .innerJoin(tujuan, eq(sasaran.tujuanId, tujuan.id))
+        .innerJoin(cascading, eq(tujuan.cascadingId, cascading.id));
+
+      const renstraRecords = await db.query.renstra.findMany({
+        where: eq(renstra.cascadingId, cascadingRecord[0].id),
+      });
+
+      await Promise.all(
+        renstraRecords.map((renstra) =>
+          Promise.all(
+            Array.from(
+              {
+                length:
+                  cascadingRecord[0].tahunBerakhir -
+                  cascadingRecord[0].tahunMulai +
+                  1,
+              },
+              (_, idx) => {
+                const tahun = cascadingRecord[0].tahunMulai + idx;
+                return db.insert(indikatorSasaranTarget).values({
+                  tahun,
+                  indikatorSasaranId: newRecord[0].id,
+                  renstraId: renstra.id,
+                });
+              },
+            ),
+          ),
+        ),
+      );
+
+      const indikatorKinerjaUtamaRecords =
+        await db.query.indikatorKinerjaUtama.findMany({
+          where: eq(indikatorKinerjaUtama.cascadingId, cascadingRecord[0].id),
+        });
+
+      await Promise.all(
+        indikatorKinerjaUtamaRecords.map((item) =>
+          db.insert(indikatorKinerjaUtamaDetail).values({
+            indikatorKinerjaUtamaId: item.id,
+            indikatorSasaranId: newRecord[0].id,
+          }),
+        ),
+      );
+    }
+
     return NextResponse.json(newRecord[0], { status: 201 });
   } catch (error) {
     console.error("Error creating 'indikator_sasaran' record: ", error);
