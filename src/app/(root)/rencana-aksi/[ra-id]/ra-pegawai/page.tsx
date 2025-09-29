6130cf1dd835cae31e6a904ded98f367ec694ed6
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-09-29 09:18:09 +0700

    change db query

--- src/app/(root)/rencana-aksi/[ra-id]/ra-pegawai/page.tsx
+++ src/app/(root)/rencana-aksi/[ra-id]/ra-pegawai/page.tsx
@@ -17,17 +17,17 @@ const RencanaAksiPegawaiPage = async ({
   });
   if (!session) redirect('/sign-in');
   const data = await db.query.rencanaAksi.findFirst({
-    where: and(
-      eq(rencanaAksi.id, parseInt(raId)),
-      eq(rencanaAksi.userId, session.user.id),
-    ),
+    where: and(eq(rencanaAksi.id, parseInt(raId))),
+    with: {
+      user: true,
+    },
   });
   return (
     <section className="flex flex-col gap-y-8 w-fit">
       <h1 className="font-semibold text-foreground text-3xl">Rencana Aksi</h1>
       <div>
         <h2>{data?.nama}</h2>
-        <p>Bagian {session?.user.name}</p>
+        <p>Bagian {data?.user.name}</p>
         <p>Tahun {data?.tahun}</p>
       </div>
       <RencanaAksiPegawaiTable />
