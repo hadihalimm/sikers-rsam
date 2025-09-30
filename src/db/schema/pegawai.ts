2ed68d1013f68e34b64be735626d5b32a4a109bb
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-09-30 13:57:49 +0700

    remove foreignkey

--- src/db/schema/pegawai.ts
+++ src/db/schema/pegawai.ts
@@ -1,19 +1,19 @@
 import { pgTable, serial, text } from 'drizzle-orm/pg-core';
-import { user } from './user';
-import { relations } from 'drizzle-orm';
+// import { user } from './user';
+// import { relations } from 'drizzle-orm';
 
 export const pegawai = pgTable('pegawai', {
   id: serial('id').primaryKey(),
   nama: text('nama').notNull(),
   jabatan: text('jabatan').notNull(),
-  userId: text('user_id')
-    .notNull()
-    .references(() => user.id, { onDelete: 'restrict' }),
+  // userId: text('user_id')
+  //   .notNull()
+  //   .references(() => user.id, { onDelete: 'restrict' }),
 });
 
-export const pegawaiRelations = relations(pegawai, ({ one }) => ({
-  user: one(user, {
-    fields: [pegawai.userId],
-    references: [user.id],
-  }),
-}));
+// export const pegawaiRelations = relations(pegawai, ({ one }) => ({
+//   user: one(user, {
+//     fields: [pegawai.userId],
+//     references: [user.id],
+//   }),
+// }));
