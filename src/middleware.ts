3f463f55fc4f3e2c3cd75f31a96e8003b3594251
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-12-16 13:34:30 +0700

    add index matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,6 +23,7 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
+    '/',
     '/cascading/:path*',
     '/renstra/:path*',
     '/indikator-kinerja-utama/:path*',
